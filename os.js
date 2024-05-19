d0dd29285f7355ad5a9e7af1b4fed1c0ce7068c3
kellystoyles68 <150394889+kellystoyles68@users.noreply.github.com>
2024-05-19 20:31:01 -0230

    new examples put in os.js file

--- os.js
+++ os.js
@@ -8,7 +8,7 @@ console.log("Architecture:" + os.arch());
 //This code will print the path to the temporary directory used by your os.
 //This method is  usefule for creating or managing temporary files or directories
 //in a way that is compatible with the user'd os.
-const tempDirectory = onseeking.tmpdir();
+const tempDirectory = os.tmpdir();
 //print the path of the tempoerary directory
 console.log("Temporary Directory:", tempDirectory);
 
@@ -20,7 +20,7 @@ try {
   console.log("Failed to set proirity for the current process", error);
 }
 
-//Example:
+//Example of an error
 const pid = 1234;
 try {
   os.setPriority(os.constants.priority.PRIORITY_LOW);
@@ -28,3 +28,12 @@ try {
 } catch (error) {
   console.log("Failed to set proirity for the current process", error);
 }
+
+// Here are some EXAMPLEs of what you can learn using the OS node
+console.log("Total Memory:", os.totalmem());
+console.log("Free Memory:", os.freemem());
+console.log("Home Directory:", os.homedir());
+console.log("Hostname:", os.hostname());
+console.log("OS release:", os.release());
+console.log("User Info:", os.userInfo());
+console.log("OS Version:", os.version());
