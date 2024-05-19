6bb3af624cfaf108cb59ab5cbe9c39a9f59c7a0f
kellystoyles68 <150394889+kellystoyles68@users.noreply.github.com>
2024-05-19 20:12:51 -0230

    completed and working OS file

--- os.js
+++ os.js
@@ -1,20 +1,17 @@
 // 0S - operating sytmen
 var os = require("os");
+
+//This code will print the name of the operating system your node.js is running on.
 console.log("platform:" + os.platform());
 console.log("Architecture:" + os.arch());
 
 //This code will print the path to the temporary directory used by your os.
 //This method is  usefule for creating or managing temporary files or directories
 //in a way that is compatible with the user'd os.
-
-//load the OS modules
-//const os = require("os");
-//get the os temporary directory
-const tempDirectory = os.tmpdir();
+const tempDirectory = onseeking.tmpdir();
 //print the path of the tempoerary directory
 console.log("Temporary Directory:", tempDirectory);
 
-//const os = require("os");
 //sets the current processes priority to the lowessst (least favorable scheduling)
 try {
   os.setPriority(os.constants.priority.PRIORITY_LOW);
