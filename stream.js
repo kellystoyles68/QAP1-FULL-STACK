346a2e226f404c33de455ed31f44a409dab32208
kellystoyles68 <150394889+kellystoyles68@users.noreply.github.com>
2024-05-19 20:05:44 -0230

    complete code for streams and working correctly

--- stream.js
+++ stream.js
@@ -1,19 +1,53 @@
 //STREAM
 //EXAMPLE:
-//const myStream = get WritablesStreamSomehow();
-//myStream.write ('some data');
-//myStream.write ('some more data');
-//myStreamEnd. ('done writing data');
+// returns a writable stream
+
+const { Writable } = require("stream");
+
+//function to get the stream write to the termina;
+function getWritablesStreamSomehow() {
+  return new Writable({
+    write(chunk, encoding, callback) {
+      console.log(chunk.toString());
+      callback();
+    },
+  });
+}
+//initializing the stream
+const myStream = getWritablesStreamSomehow();
+//writing data to stream
+myStream.write("some data");
+myStream.write("some more data");
+//ending it to the stream
+myStream.end("done writing data");
 
 //EXAMPLE:
-//getReadableStreamSomehow()
-//  .resume()
-//  .on ('end', () => {
-//  console.log ('Reached the end, but did not read anything.');
-//})
+const { Readable } = require("stream");
 
-//EXAMPLE:
-//import {Readable} from 'node:stream';
-//await Readable.from ([1,2,3,4]).drop(2).toArray();
-//Answer is [3,4]
-//you read the array and drop the first two integers [1,2] which changes the array to now be [3,4]
+//function to display to terminal
+function getReadableStreamSomehow() {
+  return new Readable({
+    read() {
+      this.push("a whole lotta data");
+      this.push("a shitload of data");
+      this.push(null);
+      console.log(getReadableStreamSomehow());
+    },
+  });
+}
+const readableStream = getReadableStreamSomehow();
+// reading data from the screen
+readableStream.on("data", (chunk) => {
+  //display to the termina;
+  console.log(chunk.toString());
+});
+
+// finished reading
+readableStream.on("end", () => {
+  console.log("Finished reading data.");
+});
+
+// handling errors
+readableStream.on("error", (err) => {
+  console.error("Error while reading from the stream:", err);
+});
